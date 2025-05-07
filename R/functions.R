48b50aed731b98e909cec8c2063d3917fead3795
Naja Helt Andersen <najahelta@hotmail.com>
2025-05-07 09:49:35 +0200

    Added Convert map to function and added import csv file function

--- R/functions.R
+++ R/functions.R
@@ -13,3 +13,20 @@ import_dime <- function(file_path) {
     )
   return(data)
 }
+
+#' Import dime csv file, so the hole dime folder with all data in it
+#'
+#' @param folder_path Path to DIME data
+#'
+#' @returns: return to all the data fram
+#'
+import_csv_files <- function(folder_path) {
+  files <- folder_path %>%
+    fs::dir_ls(glob = "*.csv")
+
+  data <- files %>%
+    purrr::map(import_dime) %>%
+    list_rbind(names_to = "file_path_id")
+
+  return(data)
+}
